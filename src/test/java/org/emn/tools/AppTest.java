6335ede86c7dfa9020152e46615c76fd3ee59bfb
ugo <ugo.lecrivain@gozmail.bzh>
2020-05-12 16:11:04 +0200

    added fake test for ci

---
+++ src/test/java/org/emn/tools/AppTest.java
@@ -0,0 +1,17 @@
+package org.emn.tools;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
